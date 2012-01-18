b6172fbdf8d3e1a973cfbfc70143f976927d7302
Hardik Ruparel <hardik988@gmail.com>
2012-01-17 20:46:18 -0800

    added install file

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Install ruby, xclip and gems
+sudo apt-get install xclip
+sudo apt-get install ruby1.9.1
+curl http://production.cf.rubygems.org/rubygems/rubygems-1.8.12.tgz --O rubygems-1.8.12.tgz
+tar -xzvf rubygems-1.8.12.tgz
+cd rubygems-1.8.12
+sudo ruby setup.rb
+
+# Install needed gems
+sudo gem install json
+sudo gem install rest_client
+sudo gem install cloudapp_api
+sudo gem install highline
+sudo gem install encrypted_strings
+sudo gem install aws-s3
+sudo gem install mail
+sudo gem install ruby-gmail
+
+# Download and install script
+curl https://raw.github.com/hardikr/s3-rb/master/mishare.rb --O mishare
+sudo cp s3.rb /usr/bin/mishare
+sudo chmod a+x /usr/bin/mishare
+
+# Run
+mishare
